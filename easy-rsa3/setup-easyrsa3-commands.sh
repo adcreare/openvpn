1d7c600c077cf3f6baae0c20e960a780fc21ef96
David <adcreare@users.noreply.github.com>
2015-06-26 17:40:06 +1000

    format changed to match blog

--- easy-rsa3/setup-easyrsa3-commands.sh
+++ easy-rsa3/setup-easyrsa3-commands.sh
@@ -1,30 +1,38 @@
-#build the CA
+## Expects easy-rsa2 has already been downloaded https://github.com/OpenVPN/easy-rsa/tree/master/easyrsa3
+
+#step 1
+mv  vars.example to vars
+
+
+#step 2 build the CA
 ./easyrsa init-pki
 ./easyrsa build-ca nopass
 
+#step 3 - build the DH key
+./easyrsa gen-dh
 
-##clients
-#Make the request
-./easyrsa gen-req client1-lappy nopass
-
-#sign the request
-./easyrsa sign-req client client1-lappy 
-
-
-##server
+#Step 4 - Generate the OpenVPN server certificate/key 
 ./easyrsa gen-req server nopass
 ./easyrsa sign-req server server
 
-./easyrsa gen-dh
+#(Optional) Step 5 - Create static secret 
+openvpn --genkey --secret ta.key
+
+# Step 6 - Create the client keys (repeat for each client) change client1-lappy to a name that suits your machine
+./easyrsa gen-req client1-lappy nopass
+./easyrsa sign-req client client1-lappy 
 
 
-##Copy keys
+# Step 7 - Server - Copy the keys - and put them in the right folders 
 cp pki/ca.crt /etc/openvpn
-
 cp pki/issued/server.crt /etc/openvpn/
 cp pki/private/server.key /etc/openvpn/
-
 cp pki/dh.pem /etc/openvpn/
+cp ta.key /etc/openvpn/
 
-cd /etc/openvpn
-sudo openvpn --genkey --secret ta.key
+# Step 8 - Client - Copy over the following keys
+#Certificate from the CA: ca.crt
+#Client public key: client1-lappy.crt
+#Client private key: client1-lappy.key
+#(Optional) shared secret if you created on in step 5 ta.key
+
